83d57d7e40d994f2c71f5f69e62e9dde7fb5f862
redyee925 <37134109+redyee925@users.noreply.github.com>
2018-11-29 15:09:26 +0800

    Update AdafruitDHT.py

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -62,23 +62,31 @@ def post_to_mcs(payload):
 	data = response.read() 
 	conn.close() 
 
+sensor_args = { '11': Adafruit_DHT.DHT11,
+                '22': Adafruit_DHT.DHT22,
+                '2302': Adafruit_DHT.AM2302 }
+if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
+    sensor = sensor_args[sys.argv[1]]
+    pin = sys.argv[2]
+else:
+    print('Usage: sudo ./Adafruit_DHT.py [11|22|2302] <GPIO pin number>')
+    print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
+    sys.exit(1)
 while 1:
-	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-
-# Un-comment the line below to convert the temperature to Fahrenheit.
-# temperature = temperature * 9/5.0 + 32
-
-# Note that sometimes you won't get a reading and
-# the results will be null (because Linux can't
-# guarantee the timing of calls to read the sensor).
-# If this happens try again!
+	humidity, temperature= Adafruit_DHT.read_retry(sensor, pin)
+	h0, t0= Adafruit_DHT.read_retry(sensor, pin)
+	SwitchStatus = GPIO.input(24)
 	if humidity is not None and temperature is not None:
-    		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-		payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":humidity}},
-			   {"dataChnId":"Temperature","values":{"value":temperature}}]} 
+		if (SwitchStatus == 0):
+			print('Button pressed')
+		else:
+			print('Button released')
+		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+		payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},
+		{"dataChnId":"Temperature","values":{"value":t0}},{"dataChnId":"SwitchStatus","values":{"value":SwitchStatus}}]} 
 		post_to_mcs(payload)
-		time.sleep(10) 
-
+		time.sleep(1)
 	else:
-    		print('Failed to get reading. Try again!')
-    		sys.exit(1)
+		print('Failed to get reading. Try again!')
+		sys.exit(1)
+		time.sleep(1)
