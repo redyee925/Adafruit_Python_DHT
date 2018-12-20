0be4e93297de2fe5ac6631db97b2d24e63224d5c
redyee925 <37134109+redyee925@users.noreply.github.com>
2018-12-20 14:05:35 +0800

    Update AdafruitDHT.py

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -62,31 +62,16 @@ def post_to_mcs(payload):
 	data = response.read() 
 	conn.close() 
 
-sensor_args = { '11': Adafruit_DHT.DHT11,
-                '22': Adafruit_DHT.DHT22,
-                '2302': Adafruit_DHT.AM2302 }
-if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
-    sensor = sensor_args[sys.argv[1]]
-    pin = sys.argv[2]
-else:
-    print('Usage: sudo ./Adafruit_DHT.py [11|22|2302] <GPIO pin number>')
-    print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
-    sys.exit(1)
+
 while 1:
 	humidity, temperature= Adafruit_DHT.read_retry(sensor, pin)
-	h0, t0= Adafruit_DHT.read_retry(sensor, pin)
-	SwitchStatus = GPIO.input(24)
 	if humidity is not None and temperature is not None:
-		if (SwitchStatus == 0):
-			print('Button pressed')
-		else:
-			print('Button released')
 		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
 		payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},
 		{"dataChnId":"Temperature","values":{"value":t0}},{"dataChnId":"SwitchStatus","values":{"value":SwitchStatus}}]} 
 		post_to_mcs(payload)
-		time.sleep(1)
+		time.sleep(10)
 	else:
 		print('Failed to get reading. Try again!')
 		sys.exit(1)
-		time.sleep(1)
+		
